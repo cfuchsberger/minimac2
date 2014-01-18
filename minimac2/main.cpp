a84dccee34206a52a2fffd12fe4c753752d2981d
cfuchsberger <cfuchsb@umich.edu>
2014-01-18 21:08:03 +0100

    first commit

---
+++ minimac2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  minimac2
+//
+//  Created by Christian Fuchsberger on 1/18/14.
+//  Copyright (c) 2014 Christian Fuchsberger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
